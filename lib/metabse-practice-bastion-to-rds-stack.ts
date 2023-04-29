c7ada39886d4d48110e7ddb35dd6e392c0d6656a
Yudai Nakajima <kituneudon97@gmail.com>
2023-04-29 19:14:22 +0900

    踏み台サーバを作成した

--- lib/metabse-practice-bastion-to-rds-stack.ts
+++ lib/metabse-practice-bastion-to-rds-stack.ts
@@ -1,4 +1,7 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnKeyPair } from '@aws-cdk/aws-ec2';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps, Token, aws_ec2 } from 'aws-cdk-lib';
+import { Ec2Action } from 'aws-cdk-lib/aws-cloudwatch-actions';
+import { Instance, InstanceClass, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -8,12 +11,59 @@ export class MetabsePracticeBastionToRdsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MetabsePracticeBastionToRdsQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // VPC作成
+    const vpc = new Vpc(this, 'MainVPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: SubnetType.PRIVATE_ISOLATED
+        }
+      ]
     });
 
-    const topic = new sns.Topic(this, 'MetabsePracticeBastionToRdsTopic');
+    // ec2用のSecurityGroup作成
+    const ec2SecurityGroup = new aws_ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: "Allow SSH",
+      allowAllOutbound: true
+    });
+    ec2SecurityGroup.addIngressRule(
+      aws_ec2.Peer.anyIpv4(),
+      aws_ec2.Port.tcp(22),
+      'Allow SSH Access'
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // key pair作成
+    const cfnKeyPair = new aws_ec2.CfnKeyPair(this, 'CfnKeyPair', {
+      keyName: 'bastion-private-key'
+    });
+    cfnKeyPair.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+    // key pair取得コマンドの出力
+    new CfnOutput(this, 'GetSSHKeyCommand', {
+      value: `aws ssm get-parameter --name /ec2/keypair/${cfnKeyPair.getAtt('KeyPairId')} --region ${this.region} --with-decryption --query Parameter.Value --output text`,
+    });
+
+    // ec2作成
+    new aws_ec2.Instance(this, 'BastionHost', {
+      vpc,
+      vpcSubnets: { subnetType: aws_ec2.SubnetType.PUBLIC },
+      instanceType: aws_ec2.InstanceType.of(
+        aws_ec2.InstanceClass.T2,
+        aws_ec2.InstanceSize.MICRO
+      ),
+      machineImage: new aws_ec2.AmazonLinuxImage({
+        generation: aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+      }),
+      securityGroup: ec2SecurityGroup,
+      keyName: Token.asString(cfnKeyPair.ref)
+    });
   }
 }
